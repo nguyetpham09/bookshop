2b8335fc27edede91b6f18fbd41275ab7e08781b
nguyetpham09 <nguyetpham969@gmail.com>
2021-12-03 20:24:54 +0700

    resolve conflict

--- BookShop.Data/Migrations/Configuration.cs
+++ BookShop.Data/Migrations/Configuration.cs
@@ -5,6 +5,7 @@
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BookShop.Data.BookShopDbContext>
     {
