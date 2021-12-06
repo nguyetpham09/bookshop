6869d3e05749ce90b7448810c6ac7b63140fabe5
nguyetpham09 <nguyetpham969@gmail.com>
2021-12-06 22:33:50 +0700

    paging list (haven't fixed yet)

--- BookShop.Web/Api/ProductCategoryController.cs
+++ BookShop.Web/Api/ProductCategoryController.cs
@@ -4,6 +4,7 @@ using BookShop.Web.Infrastructure.Core;
 using BookShop.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 
@@ -26,15 +27,29 @@ namespace BookShop.Web.Api
 
         [Route("getall")]
         [HttpGet]
-        public HttpResponseMessage Get(HttpRequestMessage request)
+        public HttpResponseMessage Get(HttpRequestMessage request, int page, int pageSize)
         {
             return CreateHttpResponse(request, () =>
             {
+                var totalRow = 0;
+
                 var productCategory = _productCategoryService.GetAll();
 
-                var productCategoryVm = Mapper.Map<List<ProductCategoryViewModel>>(productCategory);
+                totalRow = productCategory.Count();
 
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, productCategoryVm);
+                var query = productCategory.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var productCategoryVm = Mapper.Map<List<ProductCategoryViewModel>>(query);
+
+                var pagination = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = productCategoryVm,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = totalRow / pageSize
+                };
+
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, pagination);
 
                 return response;
             });
--- BookShop.Web/app/components/product_categories/productCategoryListController.js
+++ BookShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,23 +1,32 @@
 ﻿(function (app) {
-
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService']
+    productCategoryListController.$inject = ['$scope', 'apiService'];
 
     function productCategoryListController($scope, apiService) {
-
         $scope.productCategories = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
+        $scope.getProductCagories = getProductCagories;
 
-        $scope.getProductCategories = getProductCategories;
-
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data
+        function getProductCagories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 20
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
-                console.log('Load product category failed')
-            })
-        };
+                console.log('Load productcategory failed.');
+            });
+        }
 
-        $scope.getProductCategories();
+        $scope.getProductCagories();
     }
 })(angular.module('bookshop.product_categories'))
